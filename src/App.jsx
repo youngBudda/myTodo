d5e59ad0889fd1a62e3306c469f9395047dfd78b
Yan <young69budda@gmail.com>
2023-09-26 19:16:50 +0300

    qwertyy

---
+++ src/App.jsx
@@ -0,0 +1,53 @@
+import "./App.css";
+import { useState } from "react";
+import TodoList from "./components/TodoList";
+import InputField from "./components/InputField";
+
+function App() {
+  const [todos, setTodos] = useState([]);
+  const [text, setText] = useState("");
+
+  const addTodo = () => {
+    if (text.trim().length) {
+      setTodos([
+        ...todos,
+        {
+          id: new Date().toISOString(),
+          text,
+          completed: false,
+        },
+      ]);
+      setText("");
+    }
+  };
+
+  const toggleTodoComplete = (todoId) => {
+    setTodos(
+      todos.map((todo) => {
+        if (todo.id !== todoId) return todo;
+
+        return {
+          ...todo,
+          completed: !todo.completed,
+        };
+      })
+    );
+  };
+
+  const removeTodo = (todoId) => {
+    setTodos(todos.filter((todo) => todo.id !== todoId));
+  };
+
+  return (
+    <div className="App">
+      <InputField text={text} handleInput={setText} handleSubmit={addTodo} />
+      <TodoList
+        todos={todos}
+        toggleTodoComplete={toggleTodoComplete}
+        removeTodo={removeTodo}
+      />
+    </div>
+  );
+}
+
+export default App;
